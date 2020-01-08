cce302f30cd8c6b220a5f3e50e4eb4f60b6716d7
Maximilian Ehrhardt <max.ehrhardt@hotmail.de>
2020-01-08 18:27:25 +0100

    Make node independant of joint names

--- src/rover_gazebo_joint_plugin.cpp
+++ src/rover_gazebo_joint_plugin.cpp
@@ -16,33 +16,13 @@ namespace gazebo_plugins
 class RoverGazeboJointPluginPrivate
 {
 public:
-    /// Indicates which joint
-    enum
-    {
-        DRIVE_LEFT_FRONT,
-        DRIVE_RIGHT_FRONT,
-        DRIVE_LEFT_MIDDLE,
-        DRIVE_RIGHT_MIDDLE,
-        DRIVE_LEFT_REAR,
-        DRIVE_RIGHT_REAR,
-        STEER_LEFT_FRONT,
-        STEER_RIGHT_FRONT,
-        STEER_LEFT_MIDDLE,
-        STEER_RIGHT_MIDDLE,
-        STEER_LEFT_REAR,
-        STEER_RIGHT_REAR,
-        DEPLOY_LEFT_FRONT,
-        DEPLOY_RIGHT_FRONT,
-        DEPLOY_LEFT_MIDDLE,
-        DEPLOY_RIGHT_MIDDLE,
-        DEPLOY_LEFT_REAR,
-        DEPLOY_RIGHT_REAR,
-
-    };
-
     /// Callback to be called at every simulation iteration.
     /// \param[in] _info Updated simulation info.
     void OnUpdate(const gazebo::common::UpdateInfo &_info);
+
+    /// Callback when a joint command is received
+    /// \param[in] _msg Array of joint commands.
+    void OnJointCmd(rover_msgs::msg::JointCommandArray::SharedPtr _msg);
 
     /// A pointer to the GazeboROS node.
     gazebo_ros::Node::SharedPtr ros_node_;
@@ -64,6 +44,8 @@ public:
 
     /// Last update time.
     gazebo::common::Time last_update_time_;
+
+    /// PID control for right steering control
 };
 
 RoverGazeboJointPlugin::RoverGazeboJointPlugin()
@@ -86,75 +68,12 @@ void RoverGazeboJointPlugin::Load(gazebo::physics::ModelPtr _model, sdf::Element
     // Initialize ROS node
     impl_->ros_node_ = gazebo_ros::Node::Get(_sdf);
 
-    impl_->joints_.resize(18);
+    // Get all the joints in the model
+    impl_->joints_ = impl_->model_->GetJoints();
 
-    // Read the joints
-    // Right front
-    auto drive_right_front_joint = _sdf->Get<std::string>("drive_right_front_joint", "drive_right_front_joint").first;
-    impl_->joints_[RoverGazeboJointPluginPrivate::DRIVE_RIGHT_FRONT] = _model->GetJoint(drive_right_front_joint);
-    if (!impl_->joints_[RoverGazeboJointPluginPrivate::DRIVE_RIGHT_FRONT])
+    for (auto joint : impl_->joints_)
     {
-        RCLCPP_ERROR(impl_->ros_node_->get_logger(),
-                     "Drive right front joint [%s] not found, plugin will not work.",
-                     drive_right_front_joint.c_str());
-        impl_->ros_node_.reset();
-        return;
-    }
-
-    auto deploy_right_front_joint = _sdf->Get<std::string>("deploy_right_front_joint", "deploy_right_front_joint").first;
-    impl_->joints_[RoverGazeboJointPluginPrivate::DEPLOY_RIGHT_FRONT] = _model->GetJoint(deploy_right_front_joint);
-    if (!impl_->joints_[RoverGazeboJointPluginPrivate::DEPLOY_RIGHT_FRONT])
-    {
-        RCLCPP_ERROR(impl_->ros_node_->get_logger(),
-                     "Deploy right front joint [%s] not found, plugin will not work.",
-                     deploy_right_front_joint.c_str());
-        impl_->ros_node_.reset();
-        return;
-    }
-
-    auto steer_right_front_joint = _sdf->Get<std::string>("steer_right_front_joint", "steer_right_front_joint").first;
-    impl_->joints_[RoverGazeboJointPluginPrivate::STEER_RIGHT_FRONT] = _model->GetJoint(steer_right_front_joint);
-    if (!impl_->joints_[RoverGazeboJointPluginPrivate::STEER_RIGHT_FRONT])
-    {
-        RCLCPP_ERROR(impl_->ros_node_->get_logger(),
-                     "Steer right front joint [%s] not found, plugin will not work.",
-                     steer_right_front_joint.c_str());
-        impl_->ros_node_.reset();
-        return;
-    }
-
-    // Left front
-    auto drive_left_front_joint = _sdf->Get<std::string>("drive_left_front_joint", "drive_left_front_joint").first;
-    impl_->joints_[RoverGazeboJointPluginPrivate::DRIVE_LEFT_FRONT] = _model->GetJoint(drive_left_front_joint);
-    if (!impl_->joints_[RoverGazeboJointPluginPrivate::DRIVE_LEFT_FRONT])
-    {
-        RCLCPP_ERROR(impl_->ros_node_->get_logger(),
-                     "Drive left front joint [%s] not found, plugin will not work.",
-                     drive_left_front_joint.c_str());
-        impl_->ros_node_.reset();
-        return;
-    }
-
-    auto deploy_left_front_joint = _sdf->Get<std::string>("deploy_left_front_joint", "deploy_left_front_joint").first;
-    impl_->joints_[RoverGazeboJointPluginPrivate::DEPLOY_LEFT_FRONT] = _model->GetJoint(deploy_left_front_joint);
-    if (!impl_->joints_[RoverGazeboJointPluginPrivate::DEPLOY_LEFT_FRONT])
-    {
-        RCLCPP_ERROR(impl_->ros_node_->get_logger(),
-                     "Deploy left front joint [%s] not found, plugin will not work.",
-                     deploy_left_front_joint.c_str());
-        impl_->ros_node_.reset();
-        return;
-    }
-
-    auto steer_left_front_joint = _sdf->Get<std::string>("steer_left_front_joint", "steer_left_front_joint").first;
-    impl_->joints_[RoverGazeboJointPluginPrivate::STEER_LEFT_FRONT] = _model->GetJoint(steer_left_front_joint);
-    if (!impl_->joints_[RoverGazeboJointPluginPrivate::STEER_LEFT_FRONT])
-    {
-        RCLCPP_ERROR(impl_->ros_node_->get_logger(),
-                     "Steer left front joint [%s] not found, plugin will not work.",
-                     steer_left_front_joint.c_str());
-        impl_->ros_node_.reset();
-        return;
+        RCLCPP_ERROR(impl_->ros_node_->get_logger(), joint->GetName());
     }
 
     // Update rate
@@ -181,9 +100,21 @@ void RoverGazeboJointPlugin::Reset()
 void RoverGazeboJointPluginPrivate::OnUpdate(const gazebo::common::UpdateInfo &_info)
 {
     // joints_[RoverGazeboJointPluginPrivate::DRIVE_LEFT_FRONT]->SetPosition(0, 30);
-    joints_[RoverGazeboJointPluginPrivate::DRIVE_LEFT_FRONT]->SetVelocity(0, 100);
-    joints_[RoverGazeboJointPluginPrivate::DRIVE_RIGHT_FRONT]->SetVelocity(0, 100);
-    joints_[RoverGazeboJointPluginPrivate::DEPLOY_LEFT_FRONT]->SetPosition(0, 0);
+    // joints_[RoverGazeboJointPluginPrivate::DRIVE_LEFT_FRONT]->SetVelocity(0, 100);
+    // joints_[RoverGazeboJointPluginPrivate::DRIVE_RIGHT_FRONT]->SetVelocity(0, 100);
+
+    // joints_[RoverGazeboJointPluginPrivate::DEPLOY_LEFT_FRONT]->SetPosition(0, 0);
+    // joints_[RoverGazeboJointPluginPrivate::DEPLOY_RIGHT_FRONT]->SetPosition(0, 0);
+}
+
+void RoverGazeboJointPluginPrivate::OnJointCmd(rover_msgs::msg::JointCommandArray::SharedPtr _msg)
+{
+    for (auto cmd : _msg->joint_command_array)
+    {
+        // Find joint in list of joints
+        auto joint = std::find_if(joints_.begin(), joints_.end(),
+                                  [cmd](const gazebo::physics::JointPtr &m) -> bool { return m->GetName() == cmd.name; });
+    }
 }
 
 GZ_REGISTER_MODEL_PLUGIN(RoverGazeboJointPlugin)
